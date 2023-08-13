61c517969fe8d0538468f3cee52d2e1c35322c8c
kabachuha <artemkhrapov2001@yandex.ru>
2023-08-13 12:07:08 +0300

    add main functionality

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@
 
 # Contact the authors: https://deforum.github.io/
 
-import os
+import os, traceback, shutil
 import PIL, numpy
 
 import torch
@@ -32,11 +32,36 @@ import discord
 from discord.ext import commands
 
 import requests
-import typing
+import typing, functools
 import json
 
+with open('config.json', 'r') as cfg_file:
+    config = json.loads(cfg_file.read())
+assert config is not None
+
+with open('default_settings.json', 'r') as cfg_file:
+    default_settings = json.loads(cfg_file.read())
+assert default_settings is not None
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True
+
+key = ""
+
+with open('.key.txt', 'r') as id_file:
+    key = id_file.readline()
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+def to_thread(func: typing.Callable) -> typing.Coroutine:
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        return await asyncio.to_thread(func, *args, **kwargs)
+    return wrapper
+
 # Blocking common core
-class Core:
+class Safety:
     def __init__(self):
         self.safety_checker = StableDiffusionSafetyChecker.from_pretrained(
                 "CompVis/stable-diffusion-safety-checker",
@@ -48,8 +73,6 @@ class Core:
         self.device = torch.device('cuda')
         self.safety_dtype = torch.float16
         self.pf = ProfanityFilter()
-        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=5)
-        self.default_config = ...
 
     def run_safety_checker(self, image):
         image_numpy = numpy.asarray(image)
@@ -71,21 +94,126 @@ class Core:
     def is_prompt_safe(self, text):
         return self.pf.is_clean(text)
 
-# Each worker is dispatched to its own running Auto1111 instance via the HTTP api as an asyncio task
-# nothing blocking here
+@to_thread
+def make_animation(deforum_settings):
 
-# TODO: ! add support for >1 workers!
+    global config
 
-class Worker:
-    def __init__(self, core, api_url):
-        self.core = core
-        self.api_url = api_url
+    settings_json = json.dumps(deforum_settings)
+    allowed_params = ';'.join(deforum_settings.keys()).strip(';')
 
-    def make_animation():
-        ...
+    request = {'settings_json':settings_json, 'allowed_params':allowed_params}
 
-        return ...#path string
+    try:
+        response = requests.post(config['URL'], json=request)
+        if response.status_code == 200:
+            result = response.json()['outdir']
+        else:
+            return ""
+        
+    except Exception as e:
+        traceback.print_exc()
+        print(e)
 
+    return result#path string
 
+safety = Safety()
+semaphore = asyncio.Semaphore(1) # mutex for safety checker access
 
+@to_thread
+def check_animation(animation_path):
+    global safety
+    return safety.verify_generated_video_safety(animation_path)
 
+@to_thread
+def check_words(prompts):
+    global safety
+    return safety.is_prompt_safe(prompts)
+
+# Simpifiled version of parse_key_frames from Deforum
+# this one converts format like `0:(lol), 20:(kek)` to prompts JSON
+def parse_prompts(string, filename='unknown'):
+    frames = dict()
+    for match_object in string.split(","):
+        frameParam = match_object.split(":")
+        try:
+            frame = frameParam[0]
+            frames[frame] = frameParam[1].strip()
+        except SyntaxError as e:
+            e.filename = filename
+            raise e
+    if frames == {} and len(string) != 0:
+        traceback.print_exc()
+        raise RuntimeError('Key Frame string not correctly formatted')
+    return frames
+
+def find_animation(dir):
+    for f in os.listdir(dir):
+        if f.endswith('.mp4'):
+            return f
+    return ''
+
+# Starting the bot part
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def deforum(ctx, prompts: str, cadence: int = 10):
+
+    print('Received a /deforum command!')
+    print(prompts)
+
+    global safety
+    global semaphore
+
+    chan = ctx.message.channel
+
+    print('Checking if the words are safe')
+
+    words_safe = await check_words(prompts)
+
+    if not words_safe:
+        print(f'Possible bad words detected from {ctx.message.author.name} (id {ctx.message.author.id})')
+        await ctx.reply("Your prompt seems to contain bad words which we can't process due to Discord's TOS, please edit it")
+        return
+    
+    print('Parsing prompts')
+
+    try:
+        prompts = parse_prompts(prompts)
+    except Exception as e:
+        await ctx.reply('Error parsing prompts!')
+        return
+    
+    await ctx.reply('Making a Deforum animation...')
+
+    deforum_settings = {'prompts':prompts, 'diffusion_cadence':cadence}
+
+    print('Making the animation')
+
+    async with semaphore:
+        path = await make_animation(deforum_settings)
+
+    if len(path) > 0:
+        print('Animation made. Checking safety')
+
+        async with semaphore:
+            is_safe = await check_animation(path)
+
+        if not is_safe:
+            print(f'Possible unsafe animation detected from {ctx.message.author.name} (id {ctx.message.author.id})')
+            print(f'Used prompts: {prompts}')
+            shutil.rmtree(path)
+            await ctx.reply("Possible unsafe contents detected in the animation, cannot continue")
+            return
+        
+        anim_file = find_animation(os.path.abspath(path))
+        await bot.send_file(chan, anim_file, filename="Deforum.mp4")
+        await ctx.reply('Your animation is done!')
+    else:
+        print('Failed to make an animation!')
+        await ctx.reply('Sorry, there was an error making the animation!')
+
+bot.run(key)
